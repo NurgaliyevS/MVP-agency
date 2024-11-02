4e40c665626e60c334d29b3c7fb24a38f02d05f3
NurgaliyevS <nurgasab@gmail.com>
2024-11-02 09:05:27 +0500

    upd

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 function Footer(props) {
   return (
@@ -16,10 +17,59 @@ function Footer(props) {
               <h3 className="font-bold text-xl text-black">MVP AGENCY</h3>
             </div>
             <p className="text-sm opacity-70 pl-1">Let's work together!</p>
+            <nav className="flex gap-4 mt-3 justify-center lg:justify-start">
+              <Link
+                href="https://x.com/tech_nurgaliyev"
+                target="_blank"
+                title="X tech_nurgaliyev"
+              >
+                <Image
+                  src={"/x.png"}
+                  alt="X"
+                  width={24}
+                  height={24}
+                  className="w-6 h-6"
+                />
+              </Link>
+              <Link
+                href="https://www.linkedin.com/in/sabyr-nurgaliyev-43b4a822a/"
+                target="_blank"
+                title="Linkedin sabyr-nurgaliyev"
+              >
+                <Image
+                  src={"/linkedin.svg"}
+                  alt="Linkedin"
+                  width={24}
+                  height={24}
+                  className="w-6 h-6"
+                />
+              </Link>
+              <Link
+                href="https://www.producthunt.com/@tech_nurgaliyev"
+                target="_blank"
+                title="ProductHunt tech_nurgaliyeev"
+              >
+                <Image
+                  src={"/productHunt.svg"}
+                  alt="ProductHunt"
+                  width={24}
+                  height={24}
+                  className="w-6 h-6"
+                />
+              </Link>
+            </nav>
           </div>
           <div>
             <h4 className="font-bold mb-4 text-black">Quick Links</h4>
             <ul className="space-y-2">
+              <li>
+                <Link
+                  href="mailto:nurgasab@gmail.com"
+                  className="text-sm hover:text-primary"
+                >
+                  Contact
+                </Link>
+              </li>
               <li>
                 <Link href="#work" className="text-sm hover:text-primary">
                   Work
@@ -48,14 +98,67 @@ function Footer(props) {
             </ul>
           </div>
           <div>
-            <h4 className="font-bold mb-4 text-black">Contact</h4>
+            <h4 className="font-bold mb-4 text-black">Recent Work</h4>
             <ul className="space-y-2">
               <li>
                 <Link
-                  href="mailto:nurgasab@gmail.com"
                   className="text-sm hover:text-primary"
+                  href="https://environmentaljobboards.com/"
+                  target="_blank"
+                  title="Environmental Job Boards - Home Page"
                 >
-                  nurgasab@gmail.com
+                  Environmental Job Boards
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-sm hover:text-primary"
+                  href="https://subpage.io/"
+                  target="_blank"
+                  title="Subpage.io - Home Page Builder"
+                >
+                  SubPage
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-sm hover:text-primary"
+                  href="https://pregnantmeal.com/"
+                  target="_blank"
+                  title="Pregnant Meal - Home Page"
+                >
+                  PregnantMeal
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-sm hover:text-primary"
+                  href="https://uptimefriend.com/"
+                  target="_blank"
+                  title="Uptime Friend - Home Page"
+                >
+                  UptimeFriend
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-sm hover:text-primary"
+                  href="https://tripplanss.com/"
+                  target="_blank"
+                  title="Trip Planss - Home Page"
+                >
+                  TripPlanss
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  className="text-sm hover:text-primary"
+                  href="http://weeealth.com/"
+                  target="_blank"
+                  title="Weeealth - Home Page"
+                >
+                  Weeealth
                 </Link>
               </li>
             </ul>
